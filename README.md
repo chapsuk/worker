53407b648d11c21ba667b659fde6038542809557
Maxim Krasilnikov <mak.krasilnikov@gmail.com>
2018-04-23 13:51:43 +0300

    Update eaxample

--- README.md
+++ README.md
@@ -1,29 +1,5 @@
 # Worker
 
-
 ## Example
 
-```go
-func main() {
-	// Create controll group
-	g := worker.NewGroup()
-
-	// Init workers with wrappers for implement schedule or exclusive run
-	w1 := worker.ByTicker(createWorker("worker #1"), time.Second)
-	w2 := worker.ByTimer(createWorker("worker #2"), time.Second)
-	w3 := worker.WithLock(createWorker("worker #3"), &locker{})
-	w4 := worker.ByTicker(w3, time.Second)
-
-	// Add workers to controll group
-	g.Add(w1, w2, w3, w4)
-
-	// Start each worker in separate goroutine
-	g.Run()
-
-	// Wait stop signal: SIGTERM, SIGINT
-	<-grace.ShutdownContext(context.Background()).Done()
-
-	// Stop workers and wait until all running jobs completed
-	g.Stop()
-}
-```
+see [examples](./examples)
--- examples/main.go
+++ examples/main.go
@@ -2,50 +2,76 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
 	"sync/atomic"
 	"time"
 
 	"github.com/chapsuk/grace"
 	"github.com/chapsuk/worker"
+	"github.com/go-redis/redis"
+	"go.uber.org/zap"
 )
 
-type locker struct {
-	locked uint32
-}
-
-func (l *locker) Lock() error {
-	if atomic.CompareAndSwapUint32(&l.locked, 0, 1) {
-		return nil
-	}
-	return errors.New("locked")
-}
-
-func (l *locker) Unlock() {
-	atomic.StoreUint32(&l.locked, 0)
-}
-
-func createWorker(msg string) func(context.Context) {
-	return func(ctx context.Context) {
-		log.Printf("start %s", msg)
-		time.Sleep(2 * time.Second)
-		log.Printf("stop %s", msg)
-	}
-}
-
 func main() {
-	g := worker.NewGroup()
+	log.Print("create redis client")
+	rcli := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+	})
 
-	w1 := worker.ByTicker(createWorker("ticker job"), time.Second)
-	w2 := worker.ByTimer(createWorker("timer job"), time.Second)
-	w3 := worker.WithLock(createWorker("with lock job"), &locker{})
-	w4 := worker.ByTicker(w3, time.Second)
+	if err := rcli.Ping().Err(); err != nil {
+		panic("redis ping error: " + err.Error())
+	}
 
-	g.Add(w1, w2, w3, w4)
-	g.Run()
+	log.Print("create redis lock options")
+	ropts := worker.RedisLockOptions{
+		LockKey:  "lock_key",
+		LockTTL:  5 * time.Second,
+		RedisCLI: rcli,
+		Logger:   zap.S(),
+	}
 
+	log.Print("create redis lock options for lock by bsm/redis-lock pkg")
+	opts := worker.BsmRedisLockOptions{
+		RedisLockOptions: ropts,
+		RetryCount:       3,
+		RetryDelay:       2,
+	}
+
+	log.Print("define job sample")
+	var i int32
+	job := func(ctx context.Context) {
+		atomic.AddInt32(&i, 1)
+		log.Printf("run #%d", atomic.LoadInt32(&i))
+		<-ctx.Done()
+	}
+
+	log.Print("define jobs with redis locks")
+	job1 := worker.WithBsmRedisLock(job, opts)
+	job2 := worker.WithRedisLock(job, ropts)
+
+	log.Print("create job with cron format schedule")
+	w1, err := worker.ByCronSchedule(job1, "@every 1s")
+	if err != nil {
+		panic("parse cron spec error: " + err.Error())
+	}
+
+	log.Print("create jobs with ticker schedule")
+	w2 := worker.ByTicker(job1, time.Second)
+	w3 := worker.ByTicker(job2, time.Second)
+
+	log.Print("create jobs with timer schedule")
+	w4 := worker.ByTimer(job1, time.Second)
+	w5 := worker.ByTimer(job2, time.Second)
+
+	log.Print("create workers group, init and run")
+	wk := worker.NewGroup()
+	wk.Add(w1, w2, w3, w4, w5)
+	wk.Run()
+
+	log.Print("wait stop signal")
 	<-grace.ShutdownContext(context.Background()).Done()
 
-	g.Stop()
+	log.Print("stopping")
+	wk.Stop()
+	log.Print("gracefully stopped")
 }
